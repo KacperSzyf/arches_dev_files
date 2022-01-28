df33c43df3cd00267a7f227807b7add5fde32f2f
KacperSzyf <kacper.szyf@icloud.com>
2022-01-28 10:35:51 +0000

    added media

---
+++ media/js/views/components/functions/eamena-permissions.js
@@ -0,0 +1,130 @@
+define(['knockout',
+        'viewmodels/function',
+        'viewmodels/concept-select',
+        'bindings/select2-query',
+        'views/components/widgets/concept-multiselect',
+        'views/components/simple-switch'],
+function (ko, FunctionViewModel, ConceptSelectViewModel, select2Query) {
+    return ko.components.register('views/components/functions/eamena-permissions', {
+        viewModel: function(params) {
+            FunctionViewModel.apply(this, arguments);
+            var self = this;
+            var nodegroups = {};
+            this.rerender = ko.observable(true);
+            this.cards = ko.observableArray();
+            this.nodes = ko.observableArray([]);
+            this.concepts = ko.observableArray([]);
+            this.concept_nodes = ko.observableArray();
+
+            this.concept_node = ko.observable();
+
+            this.initialUsers = ko.observableArray();
+
+            this.selected_nodegroup = params.config.selected_nodegroup;
+
+            this.selected_node = params.config.selected_node;
+       
+            this.selected_val = params.config.selected_val;
+
+            this.userGroups = params.config.user_groups;
+
+            this.triggering_nodegroups = params.config.triggering_nodegroups;
+
+            ConceptSelectViewModel.apply(this, [params]);
+
+            
+            // Use the custom /get/users endpoint to get up to date list of users. ** must add the endpoint
+            $.getJSON("http://127.0.0.1:8000/get/users", function(data){
+                // console.log("data", data)
+                self.initialUsers(data)
+                // console.log("init", self.initialUsers())
+            });
+
+            // console.log(self.initialUsers());
+
+            // Requires push method when we get to multiple rules 
+            this.selected_nodegroup.subscribe(function(val){
+                self.triggering_nodegroups([val]);
+            });
+
+            
+            // Compare initialUsers to userGroups 
+            // If they are identical, do nothing.
+            // If there is a group in initialUsers but not in UserGroups, ADD 
+            // If there is a group in userGroups but not in initialUsers, DELETE
+
+            //console.log(self.userGroups());
+
+            // This compares userGroups to initialUsers and if one is missing, add it.
+            this.initialUsers.subscribe(function(val){
+                val.forEach(function(v,i){
+                    if (self.userGroups().some((identity) => ko.unwrap(identity['identityName']) === v.name) === false){
+                        console.log("adding", v.name)
+                        var groupEntry = {
+                            identityName: ko.unwrap(v.name), 
+                            identityId: ko.unwrap(v.id), 
+                            identityType: ko.unwrap(v.type), 
+                            identityVal: ko.observable(true)
+                        }
+                        self.userGroups.push(groupEntry)
+                    }
+                })
+            });
+
+
+            // return concept list 
+            this.selected_node.subscribe(function(val){
+                var concept_node = self.graph.nodes.find(function(node){
+                    return node.nodeid === val;
+                });
+                self.concept_node(concept_node);
+            });
+
+
+            // This generates the list of nodes once nodegroup is selected
+            this.selected_nodegroup.subscribe(function(){
+                self.rerender(false); //Toggling rerender forces the node options to load in the select2 dropdown when the card changes
+                var nodes = self.graph.nodes.filter(function(node){
+                    return node.nodegroup_id === self.selected_nodegroup();
+                }).map(function(node){
+                        node.id = node.nodeid;
+                        node.text = node.name;
+                        return node;
+                });
+                // re-write nodes to only concept type nodes
+                var nodes = nodes.filter(function(node){
+                    return node.datatype === 'concept';
+                })
+                self.nodes.removeAll();   
+                self.nodes(nodes);
+                self.rerender(true);
+                self.nodes().forEach(function(node){
+            
+                })
+
+            });
+
+            // Generates the list of nodegroups/cards to be used in the drop down        
+            this.graph.cards.forEach(function(card){
+                var found = !!_.find(this.graph.nodegroups, function(nodegroup){
+                    return nodegroup.parentnodegroup_id === card.nodegroup_id
+                }, this);
+                if(!found && !(card.nodegroup_id in nodegroups)){
+                    card.id = card.nodegroup_id;
+                    card.text = card.name; // Card Names
+                    this.cards.push(card); // Add to cards array
+                    nodegroups[card.nodegroup_id] = true;
+                }
+                
+            }, this);
+
+            this.selected_nodegroup.valueHasMutated();// Forces the node value to load into the node options when the template is renderer
+   
+
+        },
+        template: {
+            require: 'text!templates/views/components/functions/eamena-permissions.htm'
+        }
+
+    });
+})
