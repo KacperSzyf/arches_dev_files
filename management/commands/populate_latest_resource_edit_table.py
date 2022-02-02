4b3dee9404dbee7a30a7bc2aaeb8f20b5bf7df5c
KacperSzyf <kacper.szyf@icloud.com>
2022-02-02 14:40:48 +0000

    added comments to code

--- management/commands/populate_latest_resource_edit_table.py
+++ management/commands/populate_latest_resource_edit_table.py
@@ -6,18 +6,25 @@ class Command(BaseCommand):
     Commands for managing datatypes
     """
     def handle(self, *args, **options):
+        #Pull all edit logs and order time decending by time
         edits = EditLog.objects.order_by('resourceinstanceid', '-timestamp').distinct('resourceinstanceid')
         
         for edit in edits:
+            #Check if edit still exists within the resource
             if ResourceInstance.objects.filter(resourceinstanceid=edit.resourceinstanceid).exists():
+                #Ensure each resource has a creation event
                 if LatestResourceEdit.objects.filter(resourceinstanceid=edit.resourceinstanceid, edittype = 'create').exists():
+                    #Check if a resource has an even other than creation
                     if LatestResourceEdit.objects.filter(resourceinstanceid=edit.resourceinstanceid).exclude(edittype = 'create'):
+                        #If so remove the existing even
                         LatestResourceEdit.objects.filter(resourceinstanceid = edit.resourceinstanceid).delete()
+                    #And replace it with the most recent edit even
                     latest_edit = LatestResourceEdit()
                     latest_edit.resourceinstanceid = edit.resourceinstanceid
                     latest_edit.timestamp = edit.timestamp
                     latest_edit.edittype = edit.edittype
                     latest_edit.save()
+                #If a resource only has a creation event, save it as a new edit type creation
                 else:
                     latest_edit = LatestResourceEdit()
                     latest_edit.resourceinstanceid = edit.resourceinstanceid
